fa7a972e6968a90cdf009d67e23679ca4fd000c3
arifbugaresa <arifyuniartofajarbugaresa@gmail.com>
2023-04-16 12:48:31 +0700

    menambahkan configurasi flowbite tailwind react

---
+++ src/components/helloWorldComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function HelloWorld() {
+    return (
+        <>
+            <h1 className="text-center text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </>
+    )
+}
